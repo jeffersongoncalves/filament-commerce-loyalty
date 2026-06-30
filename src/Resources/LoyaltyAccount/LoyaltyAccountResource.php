12aa0556a10cb32ad30f8fc1a5844302067e5316
jeffersongoncalves <gerson.simao.92@gmail.com>
2026-06-30 18:22:09 +0000

    R5: Filament admin depth — relation managers (v5)

--- src/Resources/LoyaltyAccount/LoyaltyAccountResource.php
+++ src/Resources/LoyaltyAccount/LoyaltyAccountResource.php
@@ -2,8 +2,8 @@
 
 namespace JeffersonGoncalves\FilamentCommerce\Loyalty\Resources\LoyaltyAccount;
 
-use Filament\Forms\Form;
 use Filament\Resources\Resource;
+use Filament\Schemas\Schema;
 use Filament\Tables\Table;
 use JeffersonGoncalves\Commerce\Loyalty\Models\LoyaltyAccount;
 use JeffersonGoncalves\FilamentCommerce\Loyalty\CommerceLoyaltyPlugin;
@@ -26,9 +26,9 @@ class LoyaltyAccountResource extends Resource
         }
     }
 
-    public static function form(Form $form): Form
+    public static function form(Schema $schema): Schema
     {
-        return LoyaltyAccountForm::configure($form);
+        return LoyaltyAccountForm::configure($schema);
     }
 
     public static function table(Table $table): Table
